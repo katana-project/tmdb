37585bba15f950d5e4b4602798fee1130ec8f9f5
Matouš Kučera <mk@kcra.me>
2024-05-22 17:00:18 +0200

    refactor: update dependencies, regenerate

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package tmdb
 
 //go:generate python3 _tools/schema_gen.py
-//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest --config models.cfg.yaml openapi.json
-//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest --config client.cfg.yaml openapi.json
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config models.cfg.yaml openapi.json
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config client.cfg.yaml openapi.json
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/katana-project/tmdb
 
 go 1.20
 
-require github.com/oapi-codegen/runtime v1.1.0
+require github.com/oapi-codegen/runtime v1.1.1
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
-	github.com/google/uuid v1.5.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 )
